93e88ce304a5ed1ecac8dae3b9c0604b64428b3f
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-12 17:07:19 -0500

    defined schema

--- models.js
+++ models.js
@@ -1,4 +1,34 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr');
 
+db.define('Gardener', {
+  name: {
+    type: Sequelize.STRING
+  },
+  age: {
+    type: Sequelize.INTEGER
+  }
+});
+
+db.define('Plot', {
+  size: {
+    type: Sequelize.INTEGER
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN
+  }
+});
+
+db.define('Vegetable', {
+  name: {
+    type: Sequelize.STRING
+  },
+  color: {
+    type: Sequelize.STRING
+  },
+  plantedOn: {
+    type: Sequelize.DATE
+  }
+});
+
 module.exports = db;
